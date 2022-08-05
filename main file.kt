08fe232c8b9839385f5f5cb2d3e63dc66ff83a59
Mariam Fouad <20p8966@eng.asu.edu.eg>
2022-08-05 02:01:40 +0200

    This is second version.

--- main file.kt
+++ main file.kt
@@ -1,8 +1,8 @@
 fun main(){
 //Byte, Short, Int, Long
 // Float, Double
-var number1 : Double = 10.5
-var number2 : Byte = 15
+var number1 : Double = 11.8
+var number2 : Byte = 18
 var result = number1 + number2
 println(result)
-}
+} // this is version two
